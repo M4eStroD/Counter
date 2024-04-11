8e3443a491cc7afa05dff8fe5a22141a61b340a5
M4eStroD <146201707+M4eStroD@users.noreply.github.com>
2024-04-12 01:05:50 +0500

    Initial commit

---
+++ Assets/Counter/Counter.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private float _periodIncrease = 0.5f;
+    [SerializeField] private int _valueIncrease = 1;
+    [SerializeField] private TextMeshProUGUI _counter;
+
+    private Coroutine _tempCounter;
+
+    private int _currentCount = 0;
+
+    private void Start()
+    {
+        _counter.text = _currentCount.ToString();
+    }
+
+    public void StartCounter()
+    {
+        if (_tempCounter != null)
+            StopCoroutine(_tempCounter);
+
+        _tempCounter = StartCoroutine(IncreaseCounter());
+    }
+
+    public void StopCounter()
+    {
+        if (_tempCounter != null)
+            StopCoroutine(_tempCounter);
+    }
+
+    private IEnumerator IncreaseCounter()
+    {
+        var wait = new WaitForSeconds(_periodIncrease);
+
+        while (true)
+        {
+            _currentCount += _valueIncrease;
+            _counter.text = _currentCount.ToString();
+            yield return wait;
+        }
+    }
+}
